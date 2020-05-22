221f2dae5a1b66c5bcb8509a58e82734ec98a5f8
kwonyounggu <kwon.younggu@gmail.com>
2020-05-21 23:46:11 -0400

    MAY 21 AT 23:46

---
+++ codconnect/src/main/java/com/beans/FileInfoBean.java
@@ -0,0 +1,87 @@
+package com.beans;
+
+import java.io.Serializable;
+
+public class FileInfoBean implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//P: Remittance Advice, X: Rejection, E: Regular Error, F: Individual Claims Error, B: Batch Edit Report
+	private char fType = 'A'; 
+	private int fMonth = -1; //1: A->Jan, 2: B->Feb, etc
+	//6 digit health care provider/4 digit group number for P-RA, 
+	//6 digits-the position of the file sequence for X-Rerejct,
+	//5 digits-batch control number for B-Batch Edit Report
+	private String fNumber = "0000"; 
+	private String fExt = "000";
+	
+	private String name = ""; //mroFile, which is from Content-Disposition line
+	private String fileName = "";
+	
+	public FileInfoBean()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public char getfType()
+	{
+		return fType;
+	}
+	public void setfType(char fType)
+	{
+		this.fType = fType;
+	}
+	public int getfMonth()
+	{
+		return fMonth;
+	}
+	public void setfMonth(int fMonth)
+	{
+		this.fMonth = fMonth;
+	}
+	public String getfNumber()
+	{
+		return fNumber;
+	}
+	public void setfNumber(String fNumber)
+	{
+		this.fNumber = fNumber;
+	}
+	public String getfExt()
+	{
+		return fExt;
+	}
+	public void setfExt(String fExt)
+	{
+		this.fExt = fExt;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getFileName()
+	{
+		return fileName;
+	}
+	public void setFileName(String fileName)
+	{
+		this.fileName = fileName;
+		this.fExt = fileName.split("\\.")[1];
+		this.fType = fileName.charAt(0);
+		this.fMonth = Character.getNumericValue(fileName.charAt(1)) - 9; //So A: 1... D:4
+	}
+	@Override
+	public String toString()
+	{
+		return "FileInfoBean [fType=" + fType + ", fMonth=" + fMonth + ", fNumber=" + fNumber + ", fExt=" + fExt
+				+ ", name=" + name + ", fileName=" + fileName + "]";
+	}
+	
+}
--- codconnect/src/main/java/com/servlets/UploadServlet.java
+++ codconnect/src/main/java/com/servlets/UploadServlet.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 
 import java.io.IOException;
 
+import javax.mail.internet.ContentDisposition;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
+import com.beans.FileInfoBean;
 import com.utilities.JsonUtils;
 import com.utilities.TokenUtil;
 import io.jsonwebtoken.Claims;
@@ -20,6 +22,7 @@ import io.jsonwebtoken.Claims;
 import java.util.Enumeration;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+
 /**
  * Servlet implementation class UploadServlet
  */
@@ -27,34 +30,16 @@ import java.util.stream.Collectors;
 public class UploadServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
-	
 	private static final long MRO_MAX_FILE_SIZE = 1000000L;
-
 	private Logger log = Logger.getLogger(this.getClass().getName());
-	
+
 	public UploadServlet()
 	{
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	private JSONObject validateHeaders(HttpServletRequest request) throws Exception
-	{
-		/**** Checking headers and parameters of request ********************************************
-		Enumeration<String> headerNames = request.getHeaderNames();
-		while(headerNames.hasMoreElements()) 
-		{
-		  String headerName = headerNames.nextElement();
-		  log.info("Header Name - " + headerName + ", Value - " + request.getHeader(headerName));
-		}
-		
-		Enumeration<String> params = request.getParameterNames(); 
-		while(params.hasMoreElements())
-		{
-		 String paramName = params.nextElement();
-		 log.info("Parameter Name - "+paramName+", Value - "+request.getParameter(paramName));
-		}
-		*********************************************************************************************/
-		
+	{		
 		//Supposed to request through POST not GET or other method
 		if (!request.getMethod().equalsIgnoreCase("POST")) throw new Exception("Not expected method of request.");
 		//The following check can be expanded with other files being uploaded from other known path
@@ -86,63 +71,23 @@ public class UploadServlet extends HttpServlet
 			
 			    JSONObject decodedToken = validateHeaders(request);
 		        //check fileName.ext, name, decoding errors while putting to db if logged in and db store checked through GUI
-		        //request.getInputStream() to read a binary file.
-				//reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+
 				reader = request.getReader();
 				String line = reader.readLine();
 				
+				FileInfoBean fb = new FileInfoBean();
+				//find file information in detail
 				for (int i=0; line != null; i++)
 				{
-					log.info(i + ": " + line);
-					line = reader.readLine();
-					
-					//Read
-					//beginning if ------WebKitFormBoundaryBnSIoQliBBVG1Aoa
-					//discard if space line
-					//if starting HR1 .. HR8
-					//discard if space line
-					//ending if ------WebKitFormBoundaryBnSIoQliBBVG1Aoa--
-					if (line.startsWith("---") || line.trim().isEmpty()) continue;
 					if (line.startsWith("Content-Disposition:"))
-					{
-						//Content-Disposition: form-data; name="mroFile"; filename="PD801989.795"
+					{						
+						String[] dispInfo = extractDispositionInfo(line);
+						fb.setName(dispInfo[1]);
+						fb.setFileName(dispInfo[2].toUpperCase());
+						log.info(fb.toString());
+						break;
 					}
-					else if (line.startsWith("Content-Type: "))
-					{
-						//Content-Type: application/octet-stream
-					}
-					else if (line.startsWith("HR1"))
-					{
-						
-					}
-					else if (line.startsWith("HR2"))
-					{
-						
-					}
-					else if (line.startsWith("HR3"))
-					{
-						
-					}
-					else if (line.startsWith("HR4"))
-					{
-						
-					}
-					else if (line.startsWith("HR5"))
-					{
-						
-					}
-					else if (line.startsWith("HR6"))
-					{
-						
-					}
-					else if (line.startsWith("HR7"))
-					{
-						
-					}
-					else if (line.startsWith("HR8"))
-					{
-						
-					}
+					line = reader.readLine();
 				}
 			    
 				//response.getWriter().print("SUCCESS: ");
@@ -187,4 +132,127 @@ public class UploadServlet extends HttpServlet
 		doGet(request, response);
 	}
 
+    //docstore.mik.ua/orelly/java-ent/servlet/ch04_04.htm
+	private String[] extractDispositionInfo(String line) throws IOException 
+	{ 
+		// Return the line's data as an array: disposition, name, filename 
+		String[] retval = new String[3]; 
+		
+		// Convert the line to a lowercase string without the ending \r\n 
+		// Keep the original line for error messages and for variable names. 
+		String origline = line; line = origline.toLowerCase(); 
+		
+		// Get the content disposition, should be "form-data" 
+		int start = line.indexOf("content-disposition: "); 
+		int end = line.indexOf(";"); 
+		if (start == -1 || end == -1) 
+		{ 
+			throw new IOException("Content disposition corrupt: " + origline); 
+		} 
+		
+		String disposition = line.substring(start + 21, end); 
+		if (!disposition.equals("form-data")) 
+		{ 
+			throw new IOException("Invalid content disposition: " + disposition); 
+		} 
+		
+		// Get the field name 
+		start = line.indexOf("name=\"", end); // start at last semicolon 
+		end = line.indexOf("\"", start + 7); // skip name=\" 
+		if (start == -1 || end == -1) 
+		{ 
+			throw new IOException("Content disposition corrupt: " + origline); 
+		} 
+		String name = origline.substring(start + 6, end); 
+		
+		// Get the filename, if given 
+		String filename = null; 
+		start = line.indexOf("filename=\"", end + 2); // start after name 
+		end = line.indexOf("\"", start + 10); // skip filename=\" 
+		if (start != -1 && end != -1) 
+		{ // note the != 
+			filename = origline.substring(start + 10, end); 
+			// The filename may contain a full path. Cut to just the filename. 
+			int slash = Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\')); 
+			if (slash > -1) 
+			{ 
+				filename = filename.substring(slash + 1); // past last slash 
+			} 
+			if (filename.equals("")) filename = "unknown"; // sanity check 
+		} 
+		
+		// Return a String array: disposition, name, filename 
+		
+		retval[0] = disposition; 
+		retval[1] = name; 
+		retval[2] = filename; 
+		
+		return retval;	
+	}
 }
+
+/**********************************************************************************
+ * 		        //request.getInputStream() to read a binary file.
+				//reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+ */
+
+/***********************************************************************************
+ * Parsing the Content-Disposition in the following simple way is successful.
+ *      //Content-Disposition: form-data; name="mroFile"; filename="PD801989.795"
+		String[] disposition = line.split("; ");
+		String name = disposition[1].split("=")[1];
+		String fileName = disposition[2].split("=")[1];
+		
+		log.info("name: "+name+", fileName: "+fileName);
+ ***********************************************************************************/
+
+/**** Checking headers and parameters of request ********************************************
+Enumeration<String> headerNames = request.getHeaderNames();
+while(headerNames.hasMoreElements()) 
+{
+  String headerName = headerNames.nextElement();
+  log.info("Header Name - " + headerName + ", Value - " + request.getHeader(headerName));
+}
+
+Enumeration<String> params = request.getParameterNames(); 
+while(params.hasMoreElements())
+{
+ String paramName = params.nextElement();
+ log.info("Parameter Name - "+paramName+", Value - "+request.getParameter(paramName));
+}
+*********************************************************************************************/
+/***
+  					else if (line.startsWith("HR1"))
+					{
+						
+					}
+					else if (line.startsWith("HR2"))
+					{
+						
+					}
+					else if (line.startsWith("HR3"))
+					{
+						
+					}
+					else if (line.startsWith("HR4"))
+					{
+						
+					}
+					else if (line.startsWith("HR5"))
+					{
+						
+					}
+					else if (line.startsWith("HR6"))
+					{
+						
+					}
+					else if (line.startsWith("HR7"))
+					{
+						
+					}
+					else if (line.startsWith("HR8"))
+					{
+						
+					}
+					else log.info("Discard line: "+i);
+*********/
