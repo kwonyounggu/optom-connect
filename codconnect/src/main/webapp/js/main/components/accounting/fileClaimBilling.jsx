8cc99ac2e6d6c48df9760f8259e6961321936707
kwonyounggu <kwon.younggu@gmail.com>
2020-06-15 23:42:25 -0400

    JUNE 15 AT 23:42

--- codconnect/src/main/webapp/js/main/components/accounting/fileClaimBilling.jsx
+++ codconnect/src/main/webapp/js/main/components/accounting/fileClaimBilling.jsx
@@ -6,6 +6,7 @@ import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import InputMask from "react-input-mask";
 
 const styles = (theme) =>
 (
@@ -20,7 +21,7 @@ const styles = (theme) =>
 			 }
 	}
 );
-
+const ohipMask = ["9999 - 999 - 999"];
 const EXPECTED_FILE_NAME = /(^[BEFPX]{1})+([ABCDEFGHIJKL]{1})+([0-9]{4,6})+(.\d{3})$/;
 class FileClaimBilling extends React.Component
 {
@@ -30,6 +31,19 @@ class FileClaimBilling extends React.Component
 		//console.log("INFO constructor() of fileClaimBilling.jsx: ", props);
 		this.state = 
 		{
+			ohipNumber: ""
+		}
+		this.onChange = (e) =>
+		{
+			console.log("onChange: ", e.target.value);
+			this.setState({ohipNumber: e.target.value});
+		}
+		//Validate if a lower case version code of ohip number is entered
+		this.beforeMaskedValueChange = (newState, oldState, userInput) => 
+		{
+			  let { value, selection } = newState;
+			  if (selection.start >= 20) value = value.toUpperCase();
+			  return {value, selection};
 		}
 	}
 	componentDidMount()
@@ -53,140 +67,152 @@ class FileClaimBilling extends React.Component
 					    </Typography>
 						
 					</Grid>
-					<Grid item xs={12}>
-						   <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
-        <TextField disabled id="standard-disabled" label="Disabled" defaultValue="Hello World" />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="standard-search" label="Search field" type="search" />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField id="filled-search" label="Search field" type="search" variant="filled" />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="outlined"
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="outlined"
-        />
-      </div>
-    </form>
+					<Grid item xs={12} className={classes.root}>
+				      <div>
+				        <TextField id="standard-required" label="Care Provider Number" defaultValue="123456" variant="outlined"/>
+				        <TextField id="standard-disabled" label="Service Code" defaultValue="V406C" variant="outlined"/>
+				        <TextField
+				          id="standard-password-input"
+				          label="Diagnostic Code"
+						  defaultValue="939"
+							variant="outlined"
+				        />
+				        <TextField
+				          id="standard-read-only-input"
+				          label="Service Location"
+				          defaultValue="ON"
+							variant="outlined"
+				          InputProps={{
+				            readOnly: true,
+				          }}
+				        />
+				        <TextField
+				          id="standard-number"
+				          label="Number Of Services"
+				          type="number"
+							variant="outlined"
+				          InputLabelProps={{
+				            shrink: true,
+				          }}
+				        />
+				        <TextField
+				          id="standard-helperText"
+				          label="Service Date"
+							variant="outlined"
+				        />
+				      </div>
+				      <div>
+				        <TextField
+				          required
+				          id="filled-required"
+				          label="Health Card Number"
+				          defaultValue="1234-567-890"
+				          variant="filled"
+				        />
+						 <InputMask
+            mask="9999 - 999 - 999 - aa"
+            value={this.state.ohipNumber}
+            onChange={this.onChange}
+			beforeMaskedValueChange={this.beforeMaskedValueChange}
+          >
+            {() => <TextField
+              id={"ohipNmber"}
+              label={"OHIP Card Number"}
+              name={"ohipNmber"}
+              margin="normal"
+              type="text"
+              />}
+          </InputMask>
+				        <TextField
+				          disabled
+				          id="filled-disabled"
+				          label="Disabled"
+				          defaultValue="Hello World"
+				          variant="filled"
+				        />
+				        <TextField
+				          id="filled-password-input"
+				          label="Password"
+				          type="password"
+				          autoComplete="current-password"
+				          variant="filled"
+				        />
+				        <TextField
+				          id="filled-read-only-input"
+				          label="Read Only"
+				          defaultValue="Hello World"
+				          InputProps={{
+				            readOnly: true,
+				          }}
+				          variant="filled"
+				        />
+				        <TextField
+				          id="filled-number"
+				          label="Number"
+				          type="number"
+				          InputLabelProps={{
+				            shrink: true,
+				          }}
+				          variant="filled"
+				        />
+				        <TextField id="filled-search" label="Search field" type="search" variant="filled" />
+				        <TextField
+				          id="filled-helperText"
+				          label="Helper text"
+				          defaultValue="Default Value"
+				          helperText="Some important text"
+				          variant="filled"
+				        />
+				      </div>
+				      <div>
+				        <TextField
+				          required
+				          id="outlined-required"
+				          label="Required"
+				          defaultValue="Hello World"
+				          variant="outlined"
+				        />
+				        <TextField
+				          disabled
+				          id="outlined-disabled"
+				          label="Disabled"
+				          defaultValue="Hello World"
+				          variant="outlined"
+				        />
+				        <TextField
+				          id="outlined-password-input"
+				          label="Password"
+				          type="password"
+				          autoComplete="current-password"
+				          variant="outlined"
+				        />
+				        <TextField
+				          id="outlined-read-only-input"
+				          label="Read Only"
+				          defaultValue="Hello World"
+				          InputProps={{
+				            readOnly: true,
+				          }}
+				          variant="outlined"
+				        />
+				        <TextField
+				          id="outlined-number"
+				          label="Number"
+				          type="number"
+				          InputLabelProps={{
+				            shrink: true,
+				          }}
+				          variant="outlined"
+				        />
+				        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
+				        <TextField
+				          id="outlined-helperText"
+				          label="Helper text"
+				          defaultValue="Default Value"
+				          helperText="Some important text"
+				          variant="outlined"
+				        />
+				      </div>
 					</Grid>
 
 				 </Grid>
